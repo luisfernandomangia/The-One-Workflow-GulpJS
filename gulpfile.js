51ac4f50aec3d7a0cde87b11e5b66856d5885fa6
Luis Fernando Mangia <luisfernandomangia@hotmail.com>
2017-07-23 20:46:38 -0300

    correções
    Fix sass, fix imagemin, fix html min

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
-var gulp        = require('gulp'),
-    imagemin    = require('gulp-imagemin'),
-    changed     = require('gulp-changed'),
+var gulp = require('gulp'),
+    imagemin = require('gulp-imagemin'),
+    changed = require('gulp-changed'),
     htmlmin = require('gulp-htmlmin');
     browserSync = require('browser-sync');
     cssmin = require('gulp-cssmin');
@@ -8,14 +8,16 @@ var gulp        = require('gulp'),
     uglify = require('gulp-uglify');
     sass = require('gulp-sass');
     
-gulp.task('jpg', function() {
-    gulp.src('./assets/img/*.jpg')
-        .pipe(changed('./img/'))
-        .pipe(imagemin({
-            progressive: true
-        }))
-        .pipe(gulp.dest('./dist/img/'));
-});
+gulp.task('images', () =>
+    gulp.src('assets/img/*')
+        .pipe(imagemin([
+    imagemin.gifsicle({interlaced: true}),
+    imagemin.jpegtran({progressive: true}),
+    imagemin.optipng({optimizationLevel: 5}),
+    imagemin.svgo({plugins: [{removeViewBox: true}]})
+]))
+        .pipe(gulp.dest('dist/img'))
+);
  
 gulp.task('browser-sync', function() {
     browserSync.init(['./dist/**'], {
@@ -26,19 +28,19 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('sass', function() {
-    gulp.src('assets/sass/*.scss')
+  return  gulp.src('./assets/sass/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist/css/'))
+        .pipe(gulp.dest('./assets/css/'))
 }); 
 
 gulp.task('minify', function() {
   return gulp.src('./assets/html/*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('dist/'))
 });
 
 gulp.task('css-min', function () {
-    gulp.src('assets/css/*.css')
+    return gulp.src('./assets/css/*.css')
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('dist/css'));
@@ -51,7 +53,8 @@ gulp.task('compress', function() {
     .pipe(gulp.dest('dist/js'));
 });
  
-gulp.task('watch',['browser-sync','minify', 'css-min', 'compress', 'sass'], function () {
+gulp.task('watch',['browser-sync','minify', 'css-min', 'compress', 'sass', 'images'], function () {
+    gulp.watch('./assets/img/*', ['images']);
     gulp.watch('./assets/js/*.js', ['compress']);
     gulp.watch('./assets/html/*.html', ['minify']);
     gulp.watch('./assets/css/*.css', ['css-min']);
